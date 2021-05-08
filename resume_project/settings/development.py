a6939273671561ee5f083c71e84ecea00cea3b53
Lior Asis <liorbm1@gmail.com>
2021-05-08 21:27:06 +0300

    fixes

---
+++ resume_project/settings/development.py
@@ -0,0 +1,13 @@
+from .base import *
+
+DEGUB = True
+
+INSTALLED_APPS += [
+
+]
+
+MIDDLEWARE += [
+
+]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
---
+++ resume_project/settings/production.py
@@ -0,0 +1,15 @@
+
+from .base import *
+
+
+import django_heroku
+#DEBUG = False
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+ALLOWED_HOSTS = ['*']
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
+
+django_heroku.settings(locals())
