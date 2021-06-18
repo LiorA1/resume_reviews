4a486fa89ef1b3c77bc0b86106e957cd7444b08b
Lior Asis <liorbm1@gmail.com>
2021-06-18 23:30:37 +0300

    add a blog app

--- resume_project/urls.py
+++ resume_project/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -31,7 +31,11 @@ urlpatterns = [
 
     path('resumes_rest/', include('resumes_rest.urls')),
 
+    path('blog/', include('blog.urls')),
+
     path('', include('accounts.urls')),
+
+    path('about/', TemplateView.as_view(template_name="about.html"), name="site_about"),
 
 ]
 
